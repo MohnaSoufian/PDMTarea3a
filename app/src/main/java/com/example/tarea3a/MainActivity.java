487abad58d0205abe15529032eea06fea19ac328
MohnaSoufian <soufianmohna@gmail.com>
2020-04-11 19:42:14 +0200

    4. Boton "Ir 1" consiga que aparezca la segunda activdad (intent explícito)

--- app/src/main/java/com/example/tarea3a/MainActivity.java
+++ app/src/main/java/com/example/tarea3a/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tarea3a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button segundaActividadBtn = (Button) findViewById(R.id.segundaActividadBtn);
+        segundaActividadBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent startIntent= new Intent(getApplicationContext(),SecondActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
